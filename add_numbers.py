7ab867ae89400aa1dbed1c0d8daa3e11b438a35c
udaykatoch28-collab <udaykatoch28@gmail.com>
2026-07-23 20:42:42 +0530

    Create add_numbers.py

---
+++ add_numbers.py
@@ -0,0 +1,7 @@
+def add_numbers(a,b):
+  return a+b
+  num1=float(input("enter first number")
+  num2=float(input("enter first number")
+  result =add_numbers(num1, num2)
+  print(f"the sum is :{result})
+  
